5a154193d8ffd6e33dd54f770ed7ac1152a53dbc
Jung Hong <junghong0512@gmail.com>
2020-03-20 00:17:39 +0900

    Some comments are added

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/"
+  uri: "http://localhost:4000/" // movieql server version 을 실행시키고, 그 localhost 를 연결시킴. (서버를 만들어서 하면 서버 가동 없이 실행 가능할듯)
 });
 
 export default client;
